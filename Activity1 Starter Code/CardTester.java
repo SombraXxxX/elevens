1d5afc13fec935862e7510b70c934d5541c7f44e
SombraXxxX <43217593+SombraXxxX@users.noreply.github.com>
2019-02-27 18:38:35 -0500

    Update CardTester.java

--- Activity1 Starter Code/CardTester.java
+++ Activity1 Starter Code/CardTester.java
@@ -9,5 +9,13 @@ public class CardTester {
 	 */
 	public static void main(String[] args) {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		Card card1 = new Card("King", "Spades", 13); 
+		Card card2 = new Card("Queen", "Diamonds", 12); 
+		Card card3 = new Card("Nine", "Clubs", 9); 
+		
+		System.out.println("Does Card one match Card two? " + card1.matches(card2));
+		System.out.println("Does Card one match Card three? " + card1.matches(card3));    
+		System.out.println("Suit of Card two: " + card2.suit());
+		System.out.println("Card three: " + card3.toString());
 	}
 }
